1bd9b1854ba4c3b0ec026e1e67d251d905bcd92e
elipedro77 <elipderope@gmail.com>
2024-11-05 09:58:07 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'quantos anos tem neymar ?',
+    '32'
+)
+
+criaCartao(
+    'futebol',
+    'quantos anos tem cristiano ronaldo ?',
+    '39'
+)
+
+criaCartao(
+    'futebol',
+    'quantos anos tem Lionel Messi ?',
+    '37'
+)
+
+criaCartao(
+    'futebol',
+    'com quantos anos morreu o Rei Pele',
+    '82'
+)
